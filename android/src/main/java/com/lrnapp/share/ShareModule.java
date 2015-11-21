d4ed5c49d8a14b4658023bd7eb70ae5652a0116b
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-11-22 04:45:14 +0530

    Add twitter share

--- android/src/main/java/com/lrnapp/share/ShareModule.java
+++ android/src/main/java/com/lrnapp/share/ShareModule.java
@@ -7,11 +7,19 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.share.model.ShareLinkContent;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ShareModule extends ReactContextBaseJavaModule {
 
+    private ReactApplicationContext mReactContext;
+
     public ShareModule(ReactApplicationContext reactContext) {
         super(reactContext);
+
+        mReactContext = reactContext;
     }
 
     @Override
@@ -20,7 +28,7 @@ public class ShareModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void ShareOnFacebook(ReadableMap options) {
+    public void ShareOnFacebook(final ReadableMap options) {
         String title = options.getString("title");
         String description = options.getString("description");
         String link = options.getString("link");
@@ -45,5 +53,33 @@ public class ShareModule extends ReactContextBaseJavaModule {
         }
 
         ShareLinkContent content = builder.build();
+    }
+
+    @ReactMethod
+    public void ShareOnTwitter(final ReadableMap options) {
+
+        String text = options.getString("text");
+        String link = options.getString("link");
+        String image = options.getString("image");
+
+        TweetComposer.Builder builder = new TweetComposer.Builder(mReactContext);
+
+        if (text != null) {
+            builder.text(text);
+        }
+
+        if (link != null) {
+            try {
+                builder.url(new URL(link));
+            } catch (MalformedURLException e) {
+                return;
+            }
+        }
+
+        if (image != null) {
+            builder.image(Uri.parse(image));
+        }
+
+        builder.show();
     }
 }
